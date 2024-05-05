3a4ba20c0e5c81c08ed8565b17d1e22b1e15cdc6
maxscherbakov <max-tsh@yandex.ru>
2024-05-05 12:57:12 +0300

    The function of turning the device into a pumpkin

---
+++ main.cc
@@ -0,0 +1,26 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+
+int main() {
+
+    long unsigned int arr_size = 8UL * (1 << 30);
+    int step = 1 << 10;
+    char* array = new char[arr_size];
+
+    long unsigned int index = 0;
+
+    while (index < arr_size) {
+        array[index] = 0;
+
+        index += step;
+
+        if (index / step % (1 << 10) == 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+    }
+    
+    delete[] array;
+
+    return 0;
+}
